640ca27bf302759314dab3cb5fc3fdde7df46f47
Kate Lane <katelane@gmail.com>
2014-11-19 09:54:38 -0700

    initial frontend setup and structure

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
   get '/menu' => 'application#menu'
+  # resources :menu_items - ALEX let's talk
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
