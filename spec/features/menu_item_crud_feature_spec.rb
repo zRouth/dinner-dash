88c396b4b21c9af1bd2b505a21c30893b33f9fb6
Kate Lane <katelane@gmail.com>
2014-11-25 23:26:03 -0700

    menu_item crud feature tests PASSING BITCHES

--- spec/features/menu_item_crud_feature_spec.rb
+++ spec/features/menu_item_crud_feature_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "menu_items things" do
-  xscenario "User creates a new menu_item" do
+  scenario "User creates a new menu_item" do
     visit "/menu_items"
     new_menu_item = {title: "I created a menu_item", description: "full of yummy", price: 10000}
     expect(page.body).not_to include new_menu_item[:title]
@@ -9,16 +9,16 @@ feature "menu_items things" do
     fill_in "menu_item[title]", with: new_menu_item[:title]
     fill_in "menu_item[description]", with: new_menu_item[:description]
     fill_in "menu_item[price]", with: new_menu_item[:price]
-    click_link_or_button "Create menu_item"
+    click_link_or_button "Create Menu item"
     expect(page.body).to include new_menu_item[:title]
   end
 
-  xscenario "User deletes a menu_item" do
+  scenario "User deletes a menu_item" do
     visit "/menu_items"
     new_menu_item = {title: "I created a menu_item", description: "full of yummy", price: 10000}
     expect(page.body).not_to include new_menu_item[:title]
     MenuItem.create new_menu_item
-
+    visit "/menu_items"
     expect(page.body).to include new_menu_item[:title]
     click_link_or_button "Title: #{new_menu_item[:title]}"
     click_link_or_button "Delete"
@@ -26,17 +26,17 @@ feature "menu_items things" do
     expect(page.body).not_to include new_menu_item[:title]
   end
 
-  xscenario "User edits a menu_item" do
+  scenario "User edits a menu_item" do
     visit "/menu_items"
     new_menu_item = {title: "I created a menu_item", description: "full of yummy", price: 10000}
     MenuItem.create new_menu_item
-
+    visit "/menu_items"
     expect(page.body).to include new_menu_item[:title]
     click_link_or_button "Title: #{new_menu_item[:title]}"
     click_link_or_button "Edit"
 
     fill_in "menu_item[title]", with: "I edited a menu_item"
-    click_link_or_button "Update menu_item"
+    click_link_or_button "Update Menu item"
     expect(page.body).to include "I edited a menu_item"
     expect(page.body).not_to include new_menu_item[:title]
   end
