965ed9f04a13cb232edb2e1faf824691f2aeb03c
Alex <96dalex@gmail.com>
2014-12-01 09:28:37 -0700

    orders can be pickup up or delivered

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_menu_items
   has_many :menu_items, through: :order_menu_items
+
+  def pickup?
+    !delivery
+  end
+
+  def delivery?
+    delivery
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Order, :type => :model do
     order = user.orders.create
     expect(order.user.id).to eq user.id
   end
+
+  it "defaults to pickup" do
+    order = Order.new
+    expect(order.pickup?).to eq true
+    expect(order.delivery?).to eq false
+  end
+
+  it "is marked for delivery" do
+    order = Order.new(delivery: true)
+    expect(order.delivery?).to eq true
+    expect(order.pickup?).to eq false
+  end
 end
