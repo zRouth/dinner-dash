089ea2636e409da1a1a1c7bf1b46dac075da558f
Alex <96dalex@gmail.com>
2014-12-03 09:09:24 -0700

    orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,16 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all.includes(:user, order_menu_items: :menu_item)
   end
+
+  def create
+    unless current_user
+      redirect_to :back, flash[:notice] = "Please log in or sign up to view this order"
+    else
+      current_user.orders.create do |order|
+        session[:cart].each do |menu_item_id, total|
+          order.order_menu_ites.build(menu_item_id: menu_item_id, quantity: total)
+        end
+      end
+    end
+  end
 end
